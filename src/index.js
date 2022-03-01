5bd6498225dcdc321283ab467e165a167bea6b55
br-ndt <nlax76@gmail.com>
2022-03-01 13:46:58 -0500

    yarn init, webpack init, babel init, react init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.css';
+
+ReactDOM.render(
+    <h1>Hi I am the app</h1>,
+    document.getElementById('app')
+);
